63187b8e5fc46f8c7517f2d8300cb6fc966adab6
Wioleta Wrobel <129770315+WiMaW@users.noreply.github.com>
2023-11-08 09:10:27 +0100

    New places added.

--- app/src/main/java/com/example/mybeautifulcityapp/data/PlacesDataSource.kt
+++ app/src/main/java/com/example/mybeautifulcityapp/data/PlacesDataSource.kt
@@ -10,13 +10,18 @@ object PlacesDataSource {
 fun loadPlaces(): List<Place> {
     return listOf(
         Place(id = 1, R.string.city_hall, R.string.city_hall_description, R.string.city_hall_localization,R.drawable.city_hall, PlaceCategory.History),
-        Place(id = 2, R.string.former_castle, R.string.former_castle_description, R.string.former_castle_localization, R.drawable.former_castle, PlaceCategory.History),
-        Place(id = 3, R.string.eternal_love_statue, R.string.eternal_love_statue_description, R.string.eternal_love_statue_localization, R.drawable.eternal_love_statue, PlaceCategory.Art),
-        Place(id = 4, R.string.saint_michael_church, R.string.saint_michael_church_description, R.string.saint_michael_church_localization, R.drawable.saint_michael_church, PlaceCategory.History),
-        Place(id = 5, R.string.defensive_walls, R.string.defensive_walls_description, R.string.defensive_walls_localization, R.drawable.defensive_walls, PlaceCategory.History),
-        Place(id = 6, R.string.museum, R.string.museum_description, R.string.museum_localization, R.drawable.museum, PlaceCategory.History),
-        Place(id = 7, R.string.cinema, R.string.cinema_description, R.string.cinema_localization, R.drawable.cinema, PlaceCategory.Entertainment),
-        Place(id = 8, R.string.park, R.string.park_description, R.string.park_localization, R.drawable.park, PlaceCategory.Nature)
+        Place(id = 2, R.string.city_square, R.string.city_square_description,R.string.city_square_localization, R.drawable.plac_legionow, PlaceCategory.Entertainment),
+        Place(id = 3, R.string.former_castle, R.string.former_castle_description, R.string.former_castle_localization, R.drawable.former_castle, PlaceCategory.History),
+        Place(id = 4, R.string.eternal_love_statue, R.string.eternal_love_statue_description, R.string.eternal_love_statue_localization, R.drawable.eternal_love_statue, PlaceCategory.Art),
+        Place(id = 5, R.string.saint_michael_church, R.string.saint_michael_church_description, R.string.saint_michael_church_localization, R.drawable.saint_michael_church, PlaceCategory.History),
+        Place(id = 6, R.string.defensive_walls, R.string.defensive_walls_description, R.string.defensive_walls_localization, R.drawable.defensive_walls, PlaceCategory.History),
+        Place(id = 7, R.string.city_park, R.string.city_park_description, R.string.city_park_localization, R.drawable.park_center, PlaceCategory.Nature),
+        Place(id = 8, R.string.museum, R.string.museum_description, R.string.museum_localization, R.drawable.museum, PlaceCategory.History),
+        Place(id = 9, R.string.kaliska_street, R.string.kaliska_street_description, R.string.kaliska_street_localization, R.drawable.kaliska_street, PlaceCategory.Shops),
+        Place(id = 10, R.string.kaliska_gate, R.string.kaliska_gate_description, R.string.kaliska_gate_localization, R.drawable.kaliska_gate, PlaceCategory.History),
+        Place(id = 11, R.string.library, R.string.library_description, R.string.library_localization, R.drawable.library, PlaceCategory.Entertainment),
+        Place(id = 12, R.string.cinema, R.string.cinema_description, R.string.cinema_localization, R.drawable.cinema, PlaceCategory.Entertainment),
+        Place(id = 13, R.string.park, R.string.park_description, R.string.park_localization, R.drawable.park, PlaceCategory.Nature)
     )
 }
 }
